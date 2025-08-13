cb9df4894cde34ee47e3aac01b6574fefd03984d
Patrick Her <renfu.her@gmail.com>
2025-08-13 14:21:27 +0800

    新增網站圖示檔案並在佈局中加入圖示連結，提升網站品牌識別度及使用者體驗。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', '機車出租網站')</title>
+
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
     
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
