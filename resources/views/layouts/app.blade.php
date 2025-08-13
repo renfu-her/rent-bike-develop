54bc6c648b0e6f313758e6346f159cd3d6cd3cfc
Patrick Her <renfu.her@gmail.com>
2025-08-13 15:01:34 +0800

    更新jQuery的完整性檢查碼，以確保安全性與穩定性。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,7 +129,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <!-- jQuery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-    integrity="sha512-MXe5EK5gyK+fbhwQy/dukwz9fw71HZcsM4KsyDBDTvMyjymkiO0M5qqU0lF4vqLI4VnKf1+DIKf1GM6RFkO8PA=="
+    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 @stack('scripts')
